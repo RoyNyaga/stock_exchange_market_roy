3f1039e5773fc927bf73ffb70c198de0d79d134f
RoyNyaga <nyagaandreroy@gmail.com>
2022-01-27 09:21:55 +0100

    add wallet notification on symboling creationg

--- app/controllers/symbolings_controller.rb
+++ app/controllers/symbolings_controller.rb
@@ -14,7 +14,10 @@ class SymbolingsController < ApplicationController
     @symboling = Symboling.new(symboling_params)
     symbol_info = MarketstackService.get_data("eod", params[:symboling][:name])
     return render json: { message: MarketstackService.parse_error_message(symbol_info), status: "failed" } if symbol_info.keys.include?("error")
+    
     if @symboling.save
+      close_price = symbol_info["data"].first["close"]
+      Notification.create(wallet: @symboling.wallet, content: Notification.wallet_message(@symboling.name, 60)) if close_price > 60
       render json: { symboling: @symboling, status: "succeeded" }
     else
       render json: { message: @symboling.errors.full_messages, status: "failed" }
@@ -27,3 +30,5 @@ class SymbolingsController < ApplicationController
     params.require(:symboling).permit(:name, :wallet_id)
   end
 end
+
+
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,3 +1,7 @@
 class Notification < ApplicationRecord
   belongs_to :wallet
+
+  def self.wallet_message(symboling_name, threshold)
+    "The close price of #{symboling_name} is greater than #{threshold}"
+  end
 end
