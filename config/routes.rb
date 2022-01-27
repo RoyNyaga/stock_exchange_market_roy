f0236b79279d3d91a50299dca74ed1d8ffa5d897
RoyNyaga <nyagaandreroy@gmail.com>
2022-01-27 23:16:20 +0100

    add about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'wallets/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "pages#home"
+  get '/about', to: "pages#about"
+  get 'wallets/index'
   resources :wallets do 
     collection do
       get :main_page
