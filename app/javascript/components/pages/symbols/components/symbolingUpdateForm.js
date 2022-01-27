474d03150f4d9f399ac924585c071da3220ba66b
RoyNyaga <nyagaandreroy@gmail.com>
2022-01-27 20:02:41 +0100

    clean up symboling update

--- app/javascript/components/pages/symbols/components/symbolingUpdateForm.js
+++ app/javascript/components/pages/symbols/components/symbolingUpdateForm.js
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import BtnGeneral from '../../../shared/btnGeneral';
+import DisplayErrorMessages from "../../../shared/displayErrorMessages"
 import axios from 'axios';
+import Notify from '../../../shared/notify';
 
 const SymbolingUpdateForm = ({ symboling, updateSymboling, handleCancel }) => {
   const [nameInputField, setNameInputField] = useState(symboling.name)
-  
+  const [erroMessage, setErrorMessage] = useState([])
+  const [ btnLoad, setBtnLoad ] = useState(false)
+
   const submit = (e) => {
-    console.log(nameInputField)
     e.preventDefault();
+    setBtnLoad(true)
+    setErrorMessage([])
+
     axios.patch(`/symbolings/${symboling.id}`, {
       symboling: {
         name: nameInputField,
@@ -16,8 +22,14 @@ const SymbolingUpdateForm = ({ symboling, updateSymboling, handleCancel }) => {
       }
     })
     .then(response=>{
-      updateSymboling(response.data.symboling)
-      handleCancel()
+      if(response.data.status == "succeeded"){
+        updateSymboling(response.data.symboling)
+        Notify.success(`${response.data.symboling.name} was successfully added!`)
+        handleCancel()
+      }else{
+        setErrorMessage(response.data.message)
+      }
+      setBtnLoad(false)
     })
     .catch(error=>{
       console.log(error)
@@ -27,8 +39,9 @@ const SymbolingUpdateForm = ({ symboling, updateSymboling, handleCancel }) => {
   return <Wrapper>
     <input onChange={(e)=>setNameInputField(e.target.value)} defaultValue={symboling.name} type="text" className="form-control" />
     <div className="my-3 d-flex justify-content-center">
-      <BtnGeneral option={submit}>Update</BtnGeneral>
+      <BtnGeneral loading={btnLoad} option={submit}>Update</BtnGeneral>
     </div>
+    { erroMessage.length > 0 ? <DisplayErrorMessages messages={erroMessage}/> : null}
   </Wrapper>;
 }
 
